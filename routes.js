1c40be7a96140bbf24438f1d9004925e1222e162
Blake Hagen <blakehagen@gmail.com>
2016-07-20 08:44:44 -0600

    examples

--- routes.js
+++ routes.js
@@ -7,31 +7,58 @@ const BRequest = Promise.promisify(request);
 
 module.exports = (app) => {
 
-  // TEST ROUTE //
-  app.get('/api/v1/test', (req, res) => {
-    res.send('im here!');
-  });
 
-  app.get('/character/:name', (req, res) => {
-    console.log(req.params);
-
-
+  // EXAMPLE 1 //
+  app.get('/test1/', (req, res) => {
 
     let options = {
       method: 'GET',
-      uri: 'http://swapi.co/api/people/1',
-      json: true
+      uri: 'http://swapi.co/api/people/1'
     };
 
-    BRequest(options).then( response => {
-      console.log('response=======> ', response.body );
-      res.send(response.body.name);
+    BRequest(options).then(response => {
+      console.log('response ::::::::::: ==> ', response.body);
+      res.send(response.body);
+    })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
 
-    });
+  // EXAMPLE 2 //
+  app.get('/test2/', (req, res) => {
 
+    const BASE_OPTIONS = {
+      method: 'GET',
+      uri: 'http://swapi.co/api/people/'
+    };
 
+    Promise.map(_.range(1, 6), (charNum) => {
+      let options = _.cloneDeep(BASE_OPTIONS);
+      options.uri = options.uri + charNum;
+      console.log('options :::::: ==>', options);
+
+      return BRequest(options);
+    }).then((results) => {
+      console.log('results ::::: ==> ', results);
+
+      let characters = _.reduce(results, (aggregatedChars, responsePage) => {
+        console.log('responsePage.body', JSON.parse(responsePage.body));
+        return aggregatedChars.concat(JSON.parse(responsePage.body))
+      }, []);
+
+      console.log('characters :::: ==> ', characters);
+
+      res.status(200).json(characters);
+    })
+      .catch((err) => {
+        res.status(500).json(err);
+      })
 
   });
+
+
+
 
 
 
